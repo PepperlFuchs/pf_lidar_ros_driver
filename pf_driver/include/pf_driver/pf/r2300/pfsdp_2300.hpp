693f37d7f7fe5bb78e9e47718d9f991bafad8d52
wsaihopfsg <75309631+wsaihopfsg@users.noreply.github.com>
2021-01-06 18:06:50 +0800

    Update pfsdp_2300.hpp
    To correct the bug for incorrect display when start_angle and max_num_points_scan is specified

--- pf_driver/include/pf_driver/pf/r2300/pfsdp_2300.hpp
+++ pf_driver/include/pf_driver/pf/r2300/pfsdp_2300.hpp
@@ -17,7 +17,7 @@ public:
   virtual ScanParameters get_scan_parameters(int start_angle)
   {
     auto resp = get_parameter("angular_fov", "radial_range_min", "radial_range_max", "measure_start_angle",
-                              "measure_stop_angle");
+                              "measure_stop_angle", "scan_frequency");
     params.angular_fov = to_float(resp["angular_fov"]) * M_PI / 180.0;
     params.radial_range_max = to_float(resp["radial_range_max"]);
     params.radial_range_min = to_float(resp["radial_range_min"]);
@@ -26,6 +26,7 @@ public:
     params.angle_min = start_stop.first;
     params.angle_max = start_stop.second;
     get_layers_enabled(params.layers_enabled);
+    params.scan_freq = to_float(resp["scan_frequency"]);
     return params;
   }
 
