420a9374ceab301e8a3c3279f8cb1031fa649f5b
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2022-05-04 19:24:23 +0200

    cleanup launch files by using yaml files

--- pf_driver/src/ros/ros_main.cpp
+++ pf_driver/src/ros/ros_main.cpp
@@ -11,15 +11,19 @@ int main(int argc, char* argv[])
   ros::init(argc, argv, "pf_driver");
   ros::NodeHandle nh("~");
 
-  std::string transport_str, device;
+  std::string device;
+  nh.getParam("device", device);
+
+  ros::NodeHandle dev_nh("~/" + device);
+
+  std::string transport_str;
   bool init_valid = true;
   std::shared_ptr<HandleInfo> info = std::make_shared<HandleInfo>();
-  init_valid &= nh.getParam("transport", transport_str);
+  init_valid &= dev_nh.getParam("transport", transport_str);
   // selecting TCP as default if not UDP
   info->handle_type = transport_str == "udp" ? HandleInfo::HANDLE_TYPE_UDP : HandleInfo::HANDLE_TYPE_TCP;
-  init_valid &= nh.getParam("scanner_ip", info->hostname);
-  init_valid &= nh.getParam("port", info->port);
-  init_valid &= nh.getParam("device", device);
+  init_valid &= dev_nh.getParam("scanner_ip", info->hostname);
+  dev_nh.getParam("port", info->port);
 
   if (!init_valid)
   {
@@ -32,19 +36,19 @@ int main(int argc, char* argv[])
   int max_num_points_scan = 0;
   int watchdogtimeout = 0;
   bool watchdog;
-  nh.getParam("start_angle", config->start_angle);
-  nh.getParam("max_num_points_scan", max_num_points_scan);
-  nh.getParam("packet_type", config->packet_type);
-  nh.getParam("watchdogtimeout", watchdogtimeout);
-  nh.getParam("watchdogtimeout", watchdog);
+  dev_nh.getParam("start_angle", config->start_angle);
+  dev_nh.getParam("max_num_points_scan", max_num_points_scan);
+  dev_nh.getParam("packet_type", config->packet_type);
+  dev_nh.getParam("watchdogtimeout", watchdogtimeout);
+  dev_nh.getParam("watchdogtimeout", watchdog);
 
   config->max_num_points_scan = max_num_points_scan;
   config->watchdogtimeout = watchdogtimeout;
   config->watchdog = watchdog;
 
   std::string topic, frame_id;
-  nh.getParam("scan_topic", topic);
-  nh.getParam("frame_id", frame_id);
+  dev_nh.getParam("scan_topic", topic);
+  dev_nh.getParam("frame_id", frame_id);
 
   // currently ScanParameters is not set through params
   std::shared_ptr<ScanParameters> params = std::make_shared<ScanParameters>();
