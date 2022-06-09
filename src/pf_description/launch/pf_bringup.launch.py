13cbcf62a127556ffcce363d11e511bba31337a3
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2022-06-09 10:56:20 +0200

    bringup rviz
    current rviz config is not supported in ROS2

--- src/pf_description/launch/pf_bringup.launch.py
+++ src/pf_description/launch/pf_bringup.launch.py
@@ -14,13 +14,24 @@ def launch_setup(context, *args, **kwargs):
         package=package_name).find(package_name)
     scanner_description_path = os.path.join(
         pkg_share, 'urdf', scanner_arg + '_world.urdf.xacro')
+    rviz_config_path = os.path.join(pkg_share, 'config', scanner_arg + '.rviz')
 
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
+        output='screen',
         parameters=[{'robot_description': Command(
             ['xacro ', scanner_description_path])}]
     )
+
+    rviz2_node = launch_ros.actions.Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        arguments=['-d' + rviz_config_path],
+    )
+
+    return [robot_state_publisher_node, rviz2_node]
 
 
 def generate_launch_description():
--- src/pf_description/setup.py
+++ src/pf_description/setup.py
@@ -14,6 +14,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*')),
         (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
     ],
     install_requires=['setuptools'],
