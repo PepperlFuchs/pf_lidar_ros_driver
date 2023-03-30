24760a7b13014f9ee791acd17de14b0ea47f5a15
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2023-03-30 11:06:01 +0200

    add test to parse R2000 packet of type C - test fails currently

--- src/pf_driver/tests/parse_packet.cpp
+++ src/pf_driver/tests/parse_packet.cpp
@@ -5,10 +5,25 @@
 
 TEST(R2000Packet_TestSuite, testParseR2000Packet)
 {
-    rclcpp::init(0, nullptr);
-    auto node = std::make_shared<rclcpp::Node>("r2000_packet_parser");
-    RCLCPP_INFO(node->get_logger(), "Testing packet parsing");
+  rclcpp::init(0, nullptr);
 
-    PFInterface pf_interface(node);
-    rclcpp::shutdown();
+  // init variables for parsing
+  uint8_t* buf; // read from dump file
+  size_t buf_len = 1404;
+  rclcpp::Logger logger = rclcpp::get_logger("r2000_packet_parser");
+  std::vector<std::unique_ptr<PFPacket>> results;
+  size_t used = 0;
+
+  std::shared_ptr<Parser<PFPacket>> parser = std::unique_ptr<Parser<PFPacket>>(new PFR2000_C_Parser);
+  bool isParsed = false;
+
+  try{
+    parser->parse(buf, buf_len, results, used, logger);
+  } catch(std::exception &e) {
+    RCLCPP_ERROR(logger, "Parsing failed");
+  }
+
+  EXPECT_EQ(isParsed, true);
+
+  rclcpp::shutdown();
 }
