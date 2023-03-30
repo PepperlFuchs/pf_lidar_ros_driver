d61f7fb03a465246de2372e87d8c37fd1aea497c
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2023-03-30 17:01:55 +0200

    update parser test to read dump

---
+++ src/pf_driver/include/pf_driver/test_helper.h
@@ -0,0 +1,38 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+#include <ament_index_cpp/get_package_share_directory.hpp>
+
+std::vector<uint8_t> hex_to_bytes(const std::string& hex) {
+  std::vector<uint8_t> bytes;
+  for (unsigned int i = 0; i < hex.length(); i += 2) {
+    std::string byteString = hex.substr(i, 2);
+    uint8_t byte = (uint8_t) strtol(byteString.c_str(), nullptr, 16);
+    bytes.push_back(byte);
+  }
+  return bytes;
+}
+
+std::string get_dump_path()
+{
+  std::string dump_dir = ament_index_cpp::get_package_share_directory("pf_driver") + "/dumps/";
+  return dump_dir;
+}
+
+std::vector<uint8_t> read_dump(std::string& FILENAME)
+{
+  std::string filepath = get_dump_path() + FILENAME;
+  std::ifstream file(filepath, std::fstream::in);
+  std::string line = "";
+  std::vector<uint8_t> vec;
+  if (file.is_open())
+  {
+    std::getline(file, line);
+    std::cout << line << std::endl;
+    vec = hex_to_bytes(line);
+
+    file.close();
+  }
+  return vec;
+}
--- src/pf_driver/tests/parse_packet.cpp
+++ src/pf_driver/tests/parse_packet.cpp
@@ -2,24 +2,30 @@
 #include <gtest/gtest.h>
 
 #include "pf_driver/pf/pf_interface.h"
+#include "pf_driver/test_helper.h"
 
 TEST(R2000Packet_TestSuite, testParseR2000Packet)
 {
   rclcpp::init(0, nullptr);
 
   // init variables for parsing
-  uint8_t* buf;  // read from dump file
-  size_t buf_len = 1404;
+  std::string filename = "dump_r2000_C.txt";
+  std::vector<uint8_t> buffer = read_dump(filename);
+  uint8_t* buf = buffer.data();
+  size_t buf_len = buffer.size();
   rclcpp::Logger logger = rclcpp::get_logger("r2000_packet_parser");
   std::vector<std::unique_ptr<PFPacket>> results;
   size_t used = 0;
+
+  pf_interfaces::msg::PFR2000Header header_;
+  memcpy(&header_, buf, sizeof(pf_interfaces::msg::PFR2000Header));
 
   std::shared_ptr<Parser<PFPacket>> parser = std::unique_ptr<Parser<PFPacket>>(new PFR2000_C_Parser);
   bool isParsed = false;
 
   try
   {
-    parser->parse(buf, buf_len, results, used, logger);
+    isParsed = parser->parse(buf, buf_len, results, used, logger);
   }
   catch (std::exception& e)
   {
