ecaf7c7e0f83aff0730db8a12d52ecd0a1e8c4ce
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2023-05-24 16:58:23 +0200

    fix r2300 launch and yaml

--- src/pf_driver/launch/r2300.launch.py
+++ src/pf_driver/launch/r2300.launch.py
@@ -3,12 +3,14 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
     config_file = 'r2300_params.yaml'
-    correction_params_file = 'correction_params.yaml'
 
     ld = LaunchDescription()
     config = os.path.join(
@@ -16,25 +18,22 @@ def generate_launch_description():
         'config',
         config_file
         )
-    correction_params = os.path.join(
-        get_package_share_directory('pf_driver'),
-        'config',
-        correction_params_file
-        )
 
     node = Node(
         package='pf_driver',
         name='ros_main',
         executable='ros_main',
         output='screen',
-        parameters=[config, correction_params]
+        parameters=[config]
     )
 
     include_desc = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
-            FindPackageShare("pf_description"), '/launch', '/pf_bringup.launch.py'],
-            launch_arguments = {'scanner': 'r2300'}
-        )
+            FindPackageShare("pf_description"), '/launch', '/pf_bringup.launch.py'
+        ]),
+        launch_arguments = {'scanner': 'r2300'}.items()
     )
+
     ld.add_action(node)
+    ld.add_action(include_desc)
     return ld
